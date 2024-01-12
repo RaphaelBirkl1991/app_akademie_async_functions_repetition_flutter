7d2b254cbeab993178b8029b5173afb50b3c380c
raphael <raphael.birkl@gmail.com>
2024-01-12 10:09:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tasksheet_rep_async_functions
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/tasksheet_rep_async_functions/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tasksheet_rep_async_functions
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
